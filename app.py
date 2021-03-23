5d0ba7b9a88b63b4ee5041cd489d47a1acd395bc
Donoras <donoras@gmail.com>
2021-03-23 10:12:32 +0200

    Pridėtas paskyros redagavimas, nuotraukos pridėjimas, admin puslapis, puslapiavimas

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from flask_bcrypt import Bcrypt
 import forms
+from datetime import datetime
+
+import secrets
+from PIL import Image
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -19,12 +26,30 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'prisijungti'
 login_manager.login_message_category = 'info'
 
+class ManoModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.el_pastas == "el@pastas.lt"
+
 class Vartotojas(db.Model, UserMixin):
     __tablename__ = "vartotojas"
     id = db.Column(db.Integer, primary_key=True)
     vardas = db.Column("Vardas", db.String(20), unique=True, nullable=False)
     el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
+    nuotrauka = db.Column(db.String(20), nullable=False, default='default.jpg')
     slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
+
+class Irasas(db.Model):
+    __tablename__ = "irasas"
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column("Data", db.DateTime, default=datetime.now())
+    pajamos = db.Column("Pajamos", db.Boolean)
+    suma = db.Column("Vardas", db.Integer)
+    vartotojas_id = db.Column(db.Integer, db.ForeignKey("vartotojas.id"))
+    vartotojas = db.relationship("Vartotojas", lazy=True)
+
+admin = Admin(app)
+admin.add_view(ManoModelView(Vartotojas, db.session))
+admin.add_view(ManoModelView(Irasas, db.session))
 
 @login_manager.user_loader
 def load_user(vartotojo_id):
@@ -33,6 +58,7 @@ def load_user(vartotojo_id):
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/registruotis", methods=['GET', 'POST'])
 def registruotis():
@@ -72,14 +98,48 @@ def atsijungti():
 
 @app.route("/irasai")
 @login_required
-def irasai():
-    return render_template('irasai.html', title='Įrašai')
+def records():
+    db.create_all()
+    page = request.args.get('page', 1, type=int)
+    try:
+        visi_irasai = Irasas.query.filter_by(vartotojas_id=current_user.id).order_by(Irasas.data.desc()).paginate(page=page, per_page=5)
+    except:
+        visi_irasai = []
+    print(visi_irasai)
+    return render_template("irasai.html", visi_irasai=visi_irasai, datetime=datetime)
 
-@app.route("/paskyra")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profilio_nuotraukos', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/paskyra", methods=['GET', 'POST'])
 @login_required
-def account():
-    return render_template('paskyra.html', title='Paskyra')
+def paskyra():
+    form = forms.PaskyrosAtnaujinimoForma()
+    if form.validate_on_submit():
+        if form.nuotrauka.data:
+            nuotrauka = save_picture(form.nuotrauka.data)
+            current_user.nuotrauka = nuotrauka
+        current_user.vardas = form.vardas.data
+        current_user.el_pastas = form.el_pastas.data
+        db.session.commit()
+        flash('Tavo paskyra atnaujinta!', 'success')
+        return redirect(url_for('paskyra'))
+    elif request.method == 'GET':
+        form.vardas.data = current_user.vardas
+        form.el_pastas.data = current_user.el_pastas
+    nuotrauka = url_for('static', filename='profilio_nuotraukos/' + current_user.nuotrauka)
+    return render_template('paskyra.html', title='Account', form=form, nuotrauka=nuotrauka)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
     db.create_all()
+    app.run(host='127.0.0.1', port=8000, debug=True)
